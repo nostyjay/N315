8ff21ae87db4c95f8645d5e1f7c000ee3f64305f
nostyjay <54683348+nostyjay@users.noreply.github.com>
2020-10-12 03:21:31 -0400

    asdf

--- assignment3/app/app.js
+++ assignment3/app/app.js
@@ -8,14 +8,14 @@ function initListeners(){
 }
 
 function initView(){
-    $.get('/views/nav.html', function(data){
+    $.get('views/nav.html', function(data){
         $('#nav').html(data);
         initListeners();
     })
-    $.get('/views/carousel.html', function(data){
+    $.get('views/carousel.html', function(data){
         $('#carousel').html(data);
     })
-    $.get('/views/home/home.html', function(data){
+    $.get('views/home/home.html', function(data){
         $('#app').html(data);
         $('.main-carousel').flickity({
             cellAlign: 'center',
