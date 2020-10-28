ed56437b7c9054ac44febb6845ffb92c4449a8f6
nostyjay <54683348+nostyjay@users.noreply.github.com>
2020-10-28 12:33:29 -0400

    add listapp

--- finalprep/app/app.js
+++ finalprep/app/app.js
@@ -1,7 +1,63 @@
+var obj = {
+	Students: [
+		{
+			key: 'value',
+			firstName: 'Todd',
+			lastName: 'Shelton',
+			grades: [
+				{
+					className: 'n315',
+					grade: 'B'
+				},
+				{
+					className: 'n215',
+					grade: 'D'
+				}
+			]
+		},
+		{
+			key: 'value',
+			firstName: 'Tom',
+      lastName: 'Shel',
+      grades: [
+				{
+					className: 'n315',
+					grade: 'A'
+				},
+				{
+					className: 'n215',
+					grade: 'C'
+        },
+        {
+          className: 'i210',
+          grade: 'A'
+        },
+        {
+          className: 'n423',
+          grade: 'F'
+        }
+			]
+		}
+	]
+};
+
 function init() {
-  $('.navicon').click(function (e) {
-    $('nav').toggleClass('navMobileView');
-    $('.links').toggle();
-  });
+  $('.getData').click((e) => {
+    // for(let i = 0; i < obj.Students.length; i++){
+    //   console.log(obj.Students[i]);
+    // }
+
+    $.each(obj.Students, (idx, student) =>{
+        console.log(student.firstName);
+
+        $.each(student.grades, (idx, grades) =>{
+          console.log(grades.className + " " + grades.grade);
+      })
+    })
+  })
+	$('.navicon').click(function(e) {
+		$('nav').toggleClass('navMobileView');
+		$('.links').toggle();
+	});
 }
 $(document).ready(init());
